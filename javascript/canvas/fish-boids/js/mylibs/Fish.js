3ba82d80ebe06c76bb3c249b3df79cc887b59b97
trongthanh <trongthanh@gmail.com>
2012-05-17 15:34:10 +0800

    - Make fish tail wiggle by adapting the rotating phase and vertices position from original Bird boid; - Update BoidScene

--- javascript/canvas/fish-boids/js/mylibs/Fish.js
+++ javascript/canvas/fish-boids/js/mylibs/Fish.js
@@ -5,7 +5,7 @@
  */
 var Fish = function () {
 
-	THREE.PlaneGeometry.call( this, 30, 15, 2, 2 );
+	THREE.PlaneGeometry.call( this, 30, 15, 2, 1 );
 
 };
 
@@ -17,16 +17,24 @@ var FishMesh = function () {
     new Fish(),
     new THREE.MeshBasicMaterial( { map: THREE.ImageUtils.loadTexture( 'img/fish-texture.png' ), overdraw: true } )
   );
+
+  this.phase = Math.floor( Math.random() * 62.83 );
 };
 
 FishMesh.prototype = new THREE.Mesh();
 FishMesh.prototype.constructor = FishMesh;
 
 FishMesh.prototype.updateCourse = function(boid) {
-  var rotation = this.rotation,
-    boidVelocity = boid.velocity;
+  var phase = this.phase,
+      rotation = this.rotation,
+      geometry = this.geometry,
+      boidVelocity = boid.velocity;
 
   rotation.y = Math.atan2( - boidVelocity.z, boidVelocity.x );
   rotation.z = Math.asin( boidVelocity.y / boidVelocity.length() ) * 0.2; //reduce fish's vertical rotation
 
+  phase = ( phase + ( Math.max( 0, rotation.z ) + 0.1 )  ) % 62.83;
+  geometry.vertices[ 3 ].position.z = geometry.vertices[ 0 ].position.z = Math.sin( phase ) * 5;
+
+  this.phase = phase;
 };
