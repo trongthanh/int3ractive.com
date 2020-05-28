8fa049f8931216e40aae41a4c9f6af4afb50c668
Thanh Tran <trongthanh@gmail.com>
2020-05-29 00:11:32 +0700

    feat(theme): use pagination's built-in href for next and previous URL; implement dynamic data point with getter

--- _data/global.js
+++ _data/global.js
@@ -1,12 +1,15 @@
-const thisYear = new Date().getFullYear();
-const years = [];
-// my blog started in 2008
-for (let y = 2008; y <= thisYear; y++) {
-	years.push(y);
-}
-
+/* eslint-disable no-bitwise */
 module.exports = {
-	years,
+	get years() {
+		const thisYear = new Date().getFullYear();
+		const years = [];
+		// my blog started in 2008
+		for (let y = 2008; y <= thisYear; y++) {
+			years.push(y);
+		}
+
+		return years;
+	},
 	random() {
 		const segment = () => {
 			return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
