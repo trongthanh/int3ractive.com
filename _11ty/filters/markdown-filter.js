7087afa43a0ed4dddd07c356abb62c8da6c32eea
Thanh Tran <trongthanh@gmail.com>
2020-06-20 01:36:45 +0700

    feat: enable smart quotes with markdown-it typographer

--- _11ty/filters/markdown-filter.js
+++ _11ty/filters/markdown-filter.js
@@ -1,9 +1,15 @@
+// define markdown-it option in one place and use it
+// for both md library and filter
 const markdownIt = require('markdown-it')({
 	html: true,
 	breaks: true,
 	linkify: true,
+	typographer: true,
 });
 
-module.exports = function markdown(value) {
-	return markdownIt.render(value);
+module.exports = {
+	library: markdownIt,
+	filter: function markdown(value) {
+		return markdownIt.render(value);
+	},
 };
