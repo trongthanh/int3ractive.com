63b4b443c5472b6c78c3e4dca0c58aee32694a6f
Thanh Tran <trongthanh@gmail.com>
2020-06-21 23:56:33 +0700

    feat: enable adding attributes via kramdown {:...} syntax

--- _11ty/filters/markdown-filter.js
+++ _11ty/filters/markdown-filter.js
@@ -1,10 +1,19 @@
 // define markdown-it option in one place and use it
 // for both md library and filter
+const markdownItAttrs = require('markdown-it-attrs');
 const markdownIt = require('markdown-it')({
 	html: true,
 	breaks: true,
 	linkify: true,
 	typographer: true,
+});
+
+markdownIt.use(markdownItAttrs, {
+	// my old blog used kramdown syntax for additional attribute
+	// https://kramdown.gettalong.org/syntax.html#attribute-list-definitions
+	leftDelimiter: '{:',
+	rightDelimiter: '}',
+	allowedAttributes: [], // empty array = all attributes are allowed
 });
 
 module.exports = {
